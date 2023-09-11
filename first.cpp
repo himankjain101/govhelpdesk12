bd07dc5c63937b4ff4892eb177e324efa763b141
Himank Jain <himankj07@gmail.com>
2023-09-11 11:56:09 +0530

    chsnges

---
+++ first.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Enter the number A: ";
+    cin>>a;
+    // cout<<"Enter the number B: ";
+    // int b;
+    // cin>>b;
+     
+    if (a<5) {
+        cout<< "I am Himank\n" ;
+    
+    }
+    else if (a>=5 && a<10){
+        cout<<"I am ashmika\n" ;
+    }
+    else {
+        cout<< "They both are brother and sister" ;
+    } 
+
+
+}
+   
+     
+      
+
+
